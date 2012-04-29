1d6905cdee88390cd5e55bde3acd5295ec9f3fd1
Johan Andre Lundar <johan.andre.lundar@bekk.no>
2012-04-29 14:04:06 +0200

    Added all functionality to run Scheduled Agent from MainViewModel in BackgroundAgentApp

--- WP7Data.BackgroundAgentApp/ViewModel/MainViewModel.cs
+++ WP7Data.BackgroundAgentApp/ViewModel/MainViewModel.cs
@@ -1,4 +1,9 @@
+#define DEBUG_AGENT // Makes the scheduled agent run immediately for debug purposes
+
+using System;
+using System.Windows;
 using GalaSoft.MvvmLight;
+using Microsoft.Phone.Scheduler;
 
 namespace WP7Data.BackgroundAgentApp.ViewModel
 {
@@ -16,6 +21,11 @@ namespace WP7Data.BackgroundAgentApp.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
+        PeriodicTask periodicTask;
+
+        string periodicTaskName = "PeriodicAgent";
+        public bool agentsAreEnabled = true;
+        
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
@@ -29,6 +39,69 @@ namespace WP7Data.BackgroundAgentApp.ViewModel
             ////{
             ////    // Code runs "for real"
             ////}
+        }
+
+        private void StartPeriodicAgent()
+        {
+            // Variable for tracking enabled status of background agents for this app.
+            agentsAreEnabled = true;
+
+            // Obtain a reference to the period task, if one exists
+            periodicTask = ScheduledActionService.Find(periodicTaskName) as PeriodicTask;
+
+            // If the task already exists and background agents are enabled for the
+            // application, you must remove the task and then add it again to update 
+            // the schedule
+            if (periodicTask != null)
+            {
+                RemoveAgent(periodicTaskName);
+            }
+
+            periodicTask = new PeriodicTask(periodicTaskName);
+
+            // The description is required for periodic agents. This is the string that the user
+            // will see in the background services Settings page on the device.
+            periodicTask.Description = "This demonstrates a periodic task.";
+
+            // Place the call to Add in a try block in case the user has disabled agents.
+            try
+            {
+                ScheduledActionService.Add(periodicTask);
+               
+                // If debugging is enabled, use LaunchForTest to launch the agent in one minute.
+#if(DEBUG_AGENT)
+                ScheduledActionService.LaunchForTest(periodicTaskName, TimeSpan.FromSeconds(60));
+#endif
+            }
+            catch (InvalidOperationException exception)
+            {
+                if (exception.Message.Contains("BNS Error: The action is disabled"))
+                {
+                    MessageBox.Show("Background agents for this application have been disabled by the user.");
+                    agentsAreEnabled = false;
+                }
+
+                if (exception.Message.Contains("BNS Error: The maximum number of ScheduledActions of this type have already been added."))
+                {
+                    // No user action required. The system prompts the user when the hard limit of periodic tasks has been reached.
+
+                }
+            }
+            catch (SchedulerServiceException)
+            {
+                // No user action required.
+            }
+        }
+
+        private void RemoveAgent(string name)
+        {
+            try
+            {
+                ScheduledActionService.Remove(name);
+            }
+            catch (Exception)
+            {
+            }
         }
     }
 }
