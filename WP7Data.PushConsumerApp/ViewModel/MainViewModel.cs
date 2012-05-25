ac0bbcbff614f7b8033f7e2ce905caf1962610a1
Eirik Stien <eirik.stien@bekk.no>
2012-05-25 13:02:07 +0200

    removed appsettings.config as appharbor doesn't support it

--- WP7Data.PushConsumerApp/ViewModel/MainViewModel.cs
+++ WP7Data.PushConsumerApp/ViewModel/MainViewModel.cs
@@ -1,20 +1,7 @@
-using System.ComponentModel;
-using System.Diagnostics;
 using System.Windows.Threading;
-using Coding4Fun.Phone.Controls;
 using GalaSoft.MvvmLight;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Microsoft.Phone.Controls;
 using Microsoft.Phone.Notification;
 using WP7Data.Push.ConsumerApp.Model;
 using WP7Data.Push.ConsumerApp.Persistance;
@@ -140,5 +127,4 @@ namespace WP7Data.Push.ConsumerApp.ViewModel
             throw new NotImplementedException();
         }
     }
-
 }
