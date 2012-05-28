82d37f34da5fc0b049b2e49c2ffcec40c00ecea5
Eirik Stien <eirik.stien@bekk.no>
2012-05-28 15:39:12 +0200

    Service sending push messages when new users registers

--- WP7Data.PushService/PushService.svc.cs
+++ WP7Data.PushService/PushService.svc.cs
@@ -52,6 +52,9 @@ namespace WP7Data.Push.Service
             subscriber.Created = DateTime.Now;
 
             int position = _store.IsSubscribed(subscriber) ? _store.GetSubscriberPosition(subscriber) : _store.AddSubscriber(subscriber);
+
+            SendTileMessageToAllUsers(string.Format("{0} ({1}) registered as number {2}", subscriber.Nick, subscriber.Device, position) );
+
             return position;
         }
 
