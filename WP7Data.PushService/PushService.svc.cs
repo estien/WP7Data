646e10e9a9d6271d487a4e38156f7a4705a6d726
Johan Andre Lundar <johan.andre.lundar@bekk.no>
2012-05-28 14:18:30 +0200

    More try and fail

--- WP7Data.PushService/PushService.svc.cs
+++ WP7Data.PushService/PushService.svc.cs
@@ -73,7 +73,7 @@ namespace WP7Data.Push.Service
                         "</wp:Tile> " +
                         "</wp:Notification>";
 
-            string formattedTileMessage = string.Format(tileMessage, new Uri("http://2.bp.blogspot.com/_JP9OiUP__qY/TOvdF0vZpaI/AAAAAAAAAlA/5EEo_gIifD0/s1600/funny-monkey-2.jpg", UriKind.Absolute), 42, message);
+            string formattedTileMessage = string.Format(tileMessage, "http://2.bp.blogspot.com/_JP9OiUP__qY/TOvdF0vZpaI/AAAAAAAAAlA/5EEo_gIifD0/s1600/funny-monkey-2.jpg", 42, message);
 
             byte[] messageBytes = Encoding.UTF8.GetBytes(formattedTileMessage);
 
